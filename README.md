9dae40db07d7e1599d604e1244348d5de74b03c3
emmabraboke <brabokeemma@gmail.com>
2022-03-15 14:55:31 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 
+# Message Api
 message Api - https://message-03-api.herokuapp.com/
 Routes
+
+## GET
 getAllMessage - https://message-03-api.herokuapp.com/api/v1/message
+
+## POST
 createMessage - https://message-03-api.herokuapp.com/api/v1/message
+
+## GET
 getMessage - https://message-03-api.herokuapp.com/api/v1/message/:id
+
+## PATCH
 updateMessage - https://message-03-api.herokuapp.com/api/v1/message/:id
+
+## DELETE
 deleteMessage - https://message-03-api.herokuapp.com/api/v1/message/:id
 
-for update request: use PATCH instead of PUT, and also only message can be updated
+## Note:
+Only message can be updated.
 
-App is hosted on Heroku
--- controller/message.js
+++ controller/message.js
@@ -1,77 +1,80 @@
-const Message = require("../model/message")
+const Message = require("../model/message");
 
-const getAllMessage = async  (req,res)=>{
-    try {
-        const messages = await Message.find({})
-        res.status(200).json({messages})
-    } catch (error) {
-        res.status(500).json(error)
+const getAllMessage = async (req, res) => {
+  try {
+    const messages = await Message.find({});
+    res.status(200).json({ messages });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const createMessage = async (req, res) => {
+  try {
+    const body = req.body;
+    const message = await Message.create(body);
+    res.status(200).json({ message });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const getMessage = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const message = await Message.findOne({ _id: taskId });
+    if (!message) {
+      return res
+        .status(404)
+        .json({ error: `message with id ${taskId} not found` });
     }
-   
-}
+    res.status(200).json({ message: message });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-const createMessage = async (req,res)=>{
-    try {
-        const body = req.body
-        const message = await Message.create(body)
-        res.status(200).json({message})
-    } catch (error) {
-        res.status(500).json(error)
+const UpdateMessage = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const { message } = req.body;
+
+    messageUpdate = await Message.findOneAndUpdate(
+      { _id: taskId },
+      { message },
+      { new: true, runValidators: true }
+    );
+
+    if (!messageUpdate) {
+      return res
+        .status(404)
+        .json({ error: `message with id ${taskId} not found` });
     }
-   
-}
+    res.status(200).json({ messageUpdate });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-const getMessage = async (req,res)=>{
-    try {
-        const {id: taskId} = req.params
-        const message = await Message.findOne({"_id": taskId})
-        if(!message){
-           return  res.status(404).json({error : `message with id ${taskId} not found`})
-        }
-        res.status(200).json({message: message})    
-    } catch (error) {
-        res.status(500).json(error)
+const deleteMessage = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const deletedMessage = await Message.findOneAndDelete({ _id: taskId });
+    if (!deletedMessage) {
+      return res
+        .status(404)
+        .json({ error: `message with id ${taskId} not found` });
     }
-    
-}
-
-const UpdateMessage = async (req,res)=>{
-    try{
-    const {id: taskId} = req.params
-    const {message} = req.body
-
-    messageUpdate = await Message.findOneAndUpdate({"_id" : taskId},{message},{new:true, runValidators: true})
-
-    if(!messageUpdate){
-        return  res.status(404).json({error : `message with id ${taskId} not found`})
-     }
-    res.status(200).json({messageUpdate})
-    }
-    catch(error){
-        res.status(500).json(error)
-    
-    }
-}
-
-const deleteMessage = async (req,res)=>{
-    try {
-        const {id: taskId} = req.params
-        const deletedMessage = await Message.findOneAndDelete({"_id" : taskId})
-        if(!deletedMessage){
-            return  res.status(404).json({error : `message with id ${taskId} not found`})
-         }
-        res.status(200).json({deletedMessage})
-}
-    catch (error) {
-        res.status(500).json(error)
-    
-    }
-} 
+    res.status(200).json({ deletedMessage });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 module.exports = {
-    getAllMessage,
-    createMessage,
-    getMessage,
-    UpdateMessage,
-    deleteMessage
-}
+  getAllMessage,
+  createMessage,
+  getMessage,
+  UpdateMessage,
+  deleteMessage,
+};
